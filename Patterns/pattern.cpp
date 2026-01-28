7acc52c8751f47bd6bb057f9796c7103d56bb3c4
priyanshubisht-sudo <thepriyanshubisht@gmail.com>
2026-01-28 09:12:13 +0530

    done till question 9...

--- Patterns/pattern.cpp
+++ Patterns/pattern.cpp
@@ -111,6 +111,23 @@ void print8(int n) {
     }
     
 }
+void print9(int n) {
+    
+    for (int i = 1; i <= 2*n-1; i++)
+    {
+        int stars = i;
+        if (i>n)
+            {
+                stars = 2*n-i;
+            }
+        for (int j = 1; j<=stars; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    
+}
 
 int main() {
 
@@ -124,7 +141,8 @@ int main() {
     {
         int n;
         cin >> n;
-        print8(n);
+        print9(n);
+  
     }
     
 }
