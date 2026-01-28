676659e5b2b023e991fa5009ca43344c06207a2d
priyanshubisht-sudo <thepriyanshubisht@gmail.com>
2026-01-28 10:33:34 +0530

    done till question 12

--- Patterns/pattern.cpp
+++ Patterns/pattern.cpp
@@ -128,6 +128,43 @@ void print9(int n) {
     }
     
 }
+void print10(int n) {
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            if((i+j)%2 == 0){
+                cout << "1" << " ";
+            }
+            else{
+                cout << "0" << " ";
+            }
+        }
+        cout << endl;
+    }
+    
+}
+void print11(int n) {
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << j;
+        }
+        
+        for (int j = 1; j <= 2*(n-i); j++)
+        {
+            cout << " ";
+        }
+        for (int j = i; j >= 1; j--)
+        {
+            cout << j;
+        }
+        cout << endl;
+        
+    }
+    
+}
 
 int main() {
 
@@ -141,7 +178,7 @@ int main() {
     {
         int n;
         cin >> n;
-        print9(n);
+        print11(n);
   
     }
     
