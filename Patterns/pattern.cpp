0f864c0511634f20eccca1fb124b3672d61701b7
priyanshubisht-sudo <thepriyanshubisht@gmail.com>
2026-01-29 17:59:55 +0530

    all 21 patterns done.

--- Patterns/pattern.cpp
+++ Patterns/pattern.cpp
@@ -202,6 +202,187 @@ void print14(int n) {
     }
     
 }
+void print15(int n) {
+    for (int i = 0; i < n; i++)
+    {
+        for (char ch = 'A'; ch <= 'A' + (n-i-1); ch++)
+        {
+            cout << ch << " ";
+        }
+        cout << endl;
+    }
+    
+}
+void print16(int n) {
+    for (int i = 0; i < n; i++)
+    {
+        char ch = 'A' + i;
+        for (int j = 0; j <= i; j++)
+        {
+            cout << ch << " ";
+        }
+        cout << endl;
+    }
+    
+}
+void print17(int n) {
+    for (int i = 0; i < n; i++)
+    {
+        // space
+        for (int j = 0; j < n-i-1; j++)
+        {
+            cout << " ";
+        }
+        // char
+        char ch = 'A';
+        int breakpoint = (2*i+1)/2;
+        for (int j = 1; j <= 2*i+1; j++)
+        {
+            cout << ch;
+            if(j<= breakpoint){
+                ch++;
+            }
+            else
+            {
+                ch--;
+            }
+            
+        }
+        // space
+        for (int j = 0; j < n-i-1; j++)
+        {
+            cout << " ";
+        }
+        cout << endl;
+    }
+    
+}
+void print18(int n) {
+    for (int i = 0; i < n; i++)
+    {
+        for (char ch = 'E' - i; ch <= 'E'; ch++)
+        {
+            cout << ch << " ";
+        }
+        cout << endl;
+    }
+    
+}
+void print19(int n) {
+    // for (int i = n; i >= 1; i--)
+    // {
+    //     for (int j = 1; j <= i; j++)
+    //     {
+    //         cout << "*";
+    //     }
+    //     for (int j = 1; j <= 2*(n-i); j++)
+    //     {
+    //         cout << " ";
+    //     }
+    //     for (int j = 1; j <= i; j++)
+    //     {
+    //         cout << "*";
+    //     }
+    //     cout << endl;
+        
+    // }
+    // for (int i = 1; i <= n; i++)
+    // {
+    //     for (int j = 1; j <= i; j++)
+    //     {
+    //         cout << "*";
+    //     }
+    //     for (int j = 1; j <= 2*(n-i); j++)
+    //     {
+    //         cout << " ";
+    //     }
+    //     for (int j = i; j >= 1; j--)
+    //     {
+    //         cout << "*";
+    //     }
+    //     cout << endl;
+        
+    // }
+    int inIs = 0;
+    for (int i = 0; i < n; i++)
+    {
+        // stars
+        for (int j = 1; j <= n-i; j++)
+        {
+            cout << "*";
+        }
+        // space
+        for (int j = 0; j < inIs; j++)
+        {
+            cout << " ";
+        }
+        
+        // stars
+        for (int j = 1; j <= n-i; j++)
+        {
+            cout << "*";
+        }
+        inIs += 2;
+        cout << endl;
+    }
+    inIs = 2*n-2;
+    for (int i = 1; i <= n; i++)
+    {
+        // stars
+        for (int j = 1; j <= i; j++)
+        {
+            cout << "*";
+        }
+        // space
+        for (int j = 0; j < inIs; j++)
+        {
+            cout << " ";
+        }
+        
+        // stars
+        for (int j = 1; j <= i; j++)
+        {
+            cout << "*";
+        }
+        inIs -= 2;
+        cout << endl;
+    }
+    
+    
+}
+void print20(int n) {
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (i==0 || i == n-1 || j == 0 || j == n-1)
+            {
+                cout << "*";
+            }
+            else{
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
+    
+}
+void print21(int n) {
+    for (int i = 0; i < 2*n-1; i++)
+    {
+        for (int j = 0; j < 2*n-1; j++)
+        {
+            int top = i;
+            int bottom = j;
+            int right = (2*n-2)-j;
+            int left = (2*n-2)-i;
+            cout << (n- min(min(top, bottom), min(right, left)));
+        }
+        cout << endl;
+        
+    }
+    
+}
 int main() {
 
     // outer loop -> rows
@@ -214,7 +395,7 @@ int main() {
     {
         int n;
         cin >> n;
-        print14(n);
+        print21(n);
   
     }
     
