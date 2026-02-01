d30f2a68504207270b6f1776699f3924de539bab
priyanshubisht-sudo <thepriyanshubisht@gmail.com>
2026-02-01 20:34:42 +0530

    updated with optimal solution.

--- Basic Maths for DSA/reverse_a_no.cpp
+++ Basic Maths for DSA/reverse_a_no.cpp
@@ -1,19 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-    int n;
-    cin >> n;
-    int revNo = 0;
+// Optimal
+    int reverse(int x) {
+        int rev = 0;
 
-    while (n>0)
-    {
-        int ld = n%10;
-        n = n/10;
-        revNo = (revNo*10) + ld;
+        while (x != 0) {
+            int digit = x % 10;
+            x /= 10;
+
+            // check BEFORE multiply
+            if (rev > INT_MAX/10 || (rev == INT_MAX/10 && digit > 7)) return 0;
+            if (rev < INT_MIN/10 || (rev == INT_MIN/10 && digit < -8)) return 0;
+
+            rev = rev * 10 + digit;
+        }
+
+        return rev;
     }
+        
+int main() {
 
-    cout << revNo;
+// Brute Force
+    // int n;
+    // cin >> n;
+    // int revNo = 0;
+
+    // while (n>0)
+    // {
+    //     int ld = n%10;
+    //     n = n/10;
+    //     revNo = (revNo*10) + ld;
+    // }
+
+    // cout << revNo;
     
     return 0;
 }
