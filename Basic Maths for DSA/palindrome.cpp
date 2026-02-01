8c8d8a5dfc75c868e1d0e466a16adb182172606a
priyanshubisht-sudo <thepriyanshubisht@gmail.com>
2026-02-01 20:43:28 +0530

    updated with leetcode approach.

--- Basic Maths for DSA/palindrome.cpp
+++ Basic Maths for DSA/palindrome.cpp
@@ -1,6 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// leetcode approach (optimal)
+    bool isPalindrome(int x) {
+        if (x < 0) return false;
+        int rev = 0;
+        int dup = x;
+
+        while (x != 0) {
+            int digit = x % 10;
+            x /= 10;
+
+            // check BEFORE multiply
+            if (rev > INT_MAX/10 || (rev == INT_MAX/10 && digit > 7)) return 0;
+            if (rev < INT_MIN/10 || (rev == INT_MIN/10 && digit < -8)) return 0;
+
+            rev = rev * 10 + digit;
+        }
+        if(rev == dup){
+            return true;
+            
+        }
+        else{
+            return false;
+        }
+        
+    }
+    
 int main() {
     int n;
     cin >> n;
