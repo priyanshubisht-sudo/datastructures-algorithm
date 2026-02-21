c2450e9c900c4c2e6e66db54809f1b36d0355876
priyanshubisht-sudo <thepriyanshubisht@gmail.com>
2026-02-21 15:19:07 +0530

    509. Fibonacci Number Brute Force Approach.

---
+++ LEETCODE/fibonacci.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// 509. Fibonacci Number
+/*
+The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence, such that each number is the sum of the two preceding ones, starting from 0 and 1. That is,
+
+F(0) = 0, F(1) = 1
+F(n) = F(n - 1) + F(n - 2), for n > 1.
+Given n, calculate F(n).
+*/
+
+int fib(int n){
+    if(n<=1){
+        return n;
+    }
+    return fib(n-1) + fib(n-2);
+}
+
+int main() {
+    int n;
+    cin >> n;
+    cout << fib(n);
+
+    return 0;
+}
