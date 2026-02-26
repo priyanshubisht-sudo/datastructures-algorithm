617816b72b42b4ab98df8dbda29bb7b1bde79419
priyanshubisht-sudo <thepriyanshubisht@gmail.com>
2026-02-26 20:43:38 +0530

    optimal approach.

---
+++ Arrays/Easy Problems/largest_element.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Optimal Approach
+
+int main() {
+
+    int n; 
+    cin >> n;
+
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    int largest = arr[0];
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] > largest)
+        {
+            largest = arr[i];
+        }
+    }
+
+    cout << largest;
+    
+    return 0;
+}
