a51edd41d07921f0580e3a6f72a82e24f010d195
priyanshubisht-sudo <thepriyanshubisht@gmail.com>
2026-02-28 09:32:03 +0530

    optimal approach.

---
+++ Arrays/Easy Problems/left_rotate_by_1_place.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+void leftRotate(vector<int> &arr, int n){
+    int temp = arr[0];
+    for (int i = 1; i < n; i++)
+    {
+        arr[i-1] = arr[i];
+    }
+    arr[n-1] = temp;
+}
+
+int main() {
+
+    int n;
+    cin>>n;
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    leftRotate(arr, n);
+    for(int x : arr){
+        cout << x << " ";
+    }
+
+    return 0;
+}
