23314f70890b48b11815e59b894b7d77eccb8a5b
priyanshubisht-sudo <thepriyanshubisht@gmail.com>
2026-04-14 20:38:40 +0530

    deletion at k place in LL.

--- Linked List/Deletion/deletion_tail.cpp
+++ Linked List/Deletion/deletion_tail.cpp
@@ -1,87 +1,135 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Node{
-    public:
+class Node
+{
+public:
     int data;
-    Node* next;
+    Node *next;
 
-    public:
-    Node(int data1, Node* next1){
-        data = data1;  
+public:
+    Node(int data1, Node *next1)
+    {
+        data = data1;
         next = next1;
     }
 
-    public:
-    Node(int data1){
+public:
+    Node(int data1)
+    {
         data = data1;
         next = nullptr;
     }
 };
 
-Node* converArr2LL(vector<int> &arr){
-    Node* head = new Node(arr[0]);
-    Node* mover = head;
+Node *converArr2LL(vector<int> &arr)
+{
+    Node *head = new Node(arr[0]);
+    Node *mover = head;
     for (int i = 1; i < arr.size(); i++)
     {
-        Node* temp = new Node(arr[i]);
-        mover -> next = temp;
+        Node *temp = new Node(arr[i]);
+        mover->next = temp;
         mover = temp;
     }
     return head;
 }
 
-int lengthOfLL(Node* head){
+int lengthOfLL(Node *head)
+{
     int cnt = 0;
-    Node* temp = head;
-    while(temp!=NULL){
+    Node *temp = head;
+    while (temp != NULL)
+    {
         temp = temp->next;
         cnt++;
     }
     return cnt;
 }
 
-int searchInLL(Node* head, int val){
-    Node* temp = head;
-    while(temp!=NULL){
-        if(temp->data == val) return 1;
+int searchInLL(Node *head, int val)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        if (temp->data == val)
+            return 1;
         temp = temp->next;
     }
     return 0;
 }
 
-void print(Node* head){
-    while(head != NULL){
+void print(Node *head)
+{
+    while (head != NULL)
+    {
         cout << head->data << " ";
         head = head->next;
     }
     cout << endl;
 }
 
-Node* removeHead(Node* head){
-    if(head == NULL) return head;
-    Node* temp = head;
+Node *removeHead(Node *head)
+{
+    if (head == NULL)
+        return head;
+    Node *temp = head;
     head = head->next;
     delete temp;
     return head;
 }
 
-Node* removeTail(Node* head){
-    if(head == NULL || head->next == NULL){
+Node *removeTail(Node *head)
+{
+    if (head == NULL || head->next == NULL)
+    {
         return NULL;
     }
-    Node* temp = head;
-    while(temp->next->next != NULL){
+    Node *temp = head;
+    while (temp->next->next != NULL)
+    {
         temp = temp->next;
     }
     delete temp->next;
     temp->next = nullptr;
     return head;
 }
-int main() {
-    vector <int> arr = {2, 5, 8, 7};
-    Node* head = converArr2LL(arr);
-    head = removeTail(head);
+
+Node *deleteK(Node *head, int k)
+{
+    if (head == NULL)
+    {
+        return head;
+    }
+    if (k == 1)
+    {
+        Node *temp = head;
+        head = head->next;
+        free(temp);
+        return head;
+    }
+    int cnt = 0;
+    Node *temp = head;
+    Node *prev = NULL;
+    while (temp != NULL)
+    {
+        cnt++;
+        if (cnt == k)
+        {
+            prev->next = prev->next->next;
+            free(temp);
+            break;
+        }
+        prev = temp;
+        temp = temp->next;
+    }
+    return head;
+}
+int main()
+{
+    vector<int> arr = {2, 5, 8, 7};
+    Node *head = converArr2LL(arr);
+    head = deleteK(head, 2);
     print(head);
     return 0;
-} 
+}
